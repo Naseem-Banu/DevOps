8322a003cb200c83b2e947d9e4300bbd24858758
root <root@ip-172-31-16-67.us-west-2.compute.internal>
2016-06-13 11:24:57 -0400

    Final

--- gitpush.sh
+++ gitpush.sh
@@ -8,21 +8,25 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+#
 # adding file if exists
 if [ -f $1 ];
 then
 git add $1
 echo -e "${GREEN}File added successfully${NC}"
+#
 # committing file 
 if [ -n "$2" ];
     then
         git commit -m $2
+#
 # pushing file to github by autoauthenticating
-echo -e "${GREEN}Pushing the file${NC}"    
+echo -e "${GREEN}Pushing the file to repository${NC}"    
     git push mygithub
     else
         echo -e "${RED}Error: Please write a comment to commit file${NC}"
     fi
+# 
 else
     echo -e "${RED}Error: File does not exist${NC}"
 fi
