857733a9921047dea9599644559075d5ad531e7d
root <root@ip-172-31-16-67.us-west-2.compute.internal>
2016-06-13 11:14:44 -0400

    committing

--- gitpush.sh
+++ gitpush.sh
@@ -6,12 +6,13 @@
 #
 #
 RED='\033[0;31m'
+GREEN='\033[0;32m'
 NC='\033[0m'
 # adding file if exists
 if [ -f $1 ];
 then
 git add $1
-echo "File added"
+echo "${GREEN}File added successfully${NC}"
 # committing file 
 if [ -n "$2" ];
     then
